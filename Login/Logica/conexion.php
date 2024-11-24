e1cf265cd94ef3852691cd614c628f694595c9d6
Marckytos <marquillos.bros3451@gmail.com>
2024-11-24 00:08:57 -0600

    conexion modificada para hosting

--- Login/Logica/conexion.php
+++ Login/Logica/conexion.php
@@ -1,8 +1,14 @@
 <?php
-$host_db="localhost:3306";
-$user_db="root";
-$pass_db="toor";
-$db_name="registros";
+#$host_db="localhost:3306";
+#$user_db="root";
+#$pass_db="toor";
+#$db_name="registros";
+
+$host_db="sql100.infinityfree.com";
+$user_db="if0_37329559";
+$pass_db="Rdkct0829FZ2PXU";
+$db_name="if0_37329559_registros";
+
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
 
